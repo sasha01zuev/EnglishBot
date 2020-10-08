70565d9a109530fe3abb4ca4d0d5e8ecd2630fe3
sasha_zuev <sashazuev01@gmail.com>
2020-10-08 13:26:22 +0300

    DATABASE. Exception for deleting dictionary, when quantity user dictionaries = 0;

--- handlers/users/menu/change_dictionary.py
+++ handlers/users/menu/change_dictionary.py
@@ -1,3 +1,4 @@
+import asyncpg
 from aiogram.dispatcher.filters import Text
 from aiogram.types import Message, ReplyKeyboardRemove, CallbackQuery
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -18,16 +19,16 @@ async def change_dictionary(message: Message):
 
     show_dictionaries_keyboard = InlineKeyboardMarkup(
         inline_keyboard=[[InlineKeyboardButton(
-                text=f"{item[2]} - текущий словарь",
-                callback_data=select_dictionary_callback.new(
-                    dictionary_id=item[0],
-                    dictionary_name=item[2]))]
-            if str(item[0]) == str(current_dictionary)
-            else[InlineKeyboardButton(
-                text=item[2],
-                callback_data=select_dictionary_callback.new(
-                    dictionary_id=item[0],
-                    dictionary_name=item[2]))] for item in select_dictionaries]
+            text=f"{item[2]} - текущий словарь",
+            callback_data=select_dictionary_callback.new(
+                dictionary_id=item[0],
+                dictionary_name=item[2]))]
+                         if str(item[0]) == str(current_dictionary)
+                         else [InlineKeyboardButton(
+            text=item[2],
+            callback_data=select_dictionary_callback.new(
+                dictionary_id=item[0],
+                dictionary_name=item[2]))] for item in select_dictionaries]
     )
     show_dictionaries_keyboard.add(cancel_button)
 
@@ -43,14 +44,22 @@ async def cancel_choose_button(call: CallbackQuery):
 @dp.callback_query_handler(select_dictionary_callback.filter())
 async def changing_dictionary(call: CallbackQuery, callback_data: dict):
     name_selected_dictionary = callback_data['dictionary_name']
-    await call.answer(f'Выбран словарь "{name_selected_dictionary}"', cache_time=5)
     tg_id = call.from_user.id
     id_selected_dictionary = int(callback_data['dictionary_id'])
-    await call.message.delete()
 
-    ########################################################################
-    #                        DATABASE Queries                              #
-    await db.set_current_dictionary(tg_id, id_selected_dictionary)
-    ########################################################################
+    try:
 
-
+        ########################################################################
+        #                        DATABASE Queries                              #
+        await db.set_current_dictionary(tg_id, id_selected_dictionary)
+        ########################################################################
+        await call.answer(f'Выбран словарь "{name_selected_dictionary}"', cache_time=5)
+        await call.message.delete()
+    except asyncpg.exceptions.ForeignKeyViolationError:
+        await call.answer(cache_time=5)
+        await call.message.delete()
+        await call.message.answer("Этого словаря уже не существует!")
+    except:
+        await call.answer(cache_time=5)
+        await call.message.delete()
+        await call.message.answer("Упс, какая-то ошибка!")
--- handlers/users/menu/delete_dicitonary_items/last_dictionary.py
+++ handlers/users/menu/delete_dicitonary_items/last_dictionary.py
@@ -1,6 +1,8 @@
+import aiogram
 from aiogram.dispatcher import FSMContext
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
+from keyboards.default import menu
 from keyboards.inline.callback_data import delete_dictionary_callback, confirm_callback
 from keyboards.inline.confirm_buttons import confirm_keyboard
 
@@ -13,18 +15,27 @@ async def show_last_dictionary(call: CallbackQuery, callback_data: dict):
     await call.answer(cache_time=5)
     tg_id = call.from_user.id
     await call.message.delete()
+    try:
+        ######################################################################
+        #                            DATABASE Queries                        #
+        last_dictionary = await db.select_last_dictionary(tg_id)
+        ######################################################################
 
-    ######################################################################
-    #                            DATABASE Queries                        #
-    last_dictionary = await db.select_last_dictionary(tg_id)
-    ######################################################################
+        await call.message.answer(f'Вы действительно хотите удалить словарь '
+                                  f'"{last_dictionary[2]}?"\n'
+                                  f'Все слова записанные в этот словарь также удалаяются!',
+                                  reply_markup=confirm_keyboard)
 
-    await call.message.answer(f'Вы действительно хотите удалить словарь '
-                              f'"{last_dictionary[2]}?"\n'
-                              f'Все слова записанные в этот словарь также удалаяются!',
-                              reply_markup=confirm_keyboard)
+        await DeleteDictionary.SetDeleteDictionary.set()
 
-    await DeleteDictionary.SetDeleteDictionary.set()
+    except TypeError:
+
+        select_dictionaries = await db.select_dictionaries(tg_id)
+        if not len(select_dictionaries) > 0:
+            await call.message.answer("У вас нету словарей! Добавьте хоть один словарь.",
+                                      reply_markup=menu)
+        else:
+            await call.message.answer("Упс, какая-то ошибка!", reply_markup=menu)
 
 
 @dp.callback_query_handler(confirm_callback.filter(item='accept'), state=DeleteDictionary.SetDeleteDictionary)
@@ -33,7 +44,6 @@ async def accept_deletion(call: CallbackQuery, state: FSMContext):
     await call.message.delete()
 
     tg_id = call.from_user.id
-
     ######################################################################
     #                            DATABASE Queries                        #
     last_dictionary = await db.select_last_dictionary(tg_id)
